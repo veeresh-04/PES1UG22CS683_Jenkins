68984acceb2f62e150529466e9fd5b728fa23b1f
veeresh-04 <148187138+veeresh-04@users.noreply.github.com>
2025-03-22 16:50:05 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "Hello, JenkinsS\n";
+  cout << "I have successfully built and ran this  \n";
   return 0;
 }
